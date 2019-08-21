6dfd6d5a83cf66aa09807b957111f65043eb45f4
Lykrast <aymericpm@gmail.com>
2019-08-21 17:02:06 +0200

    Other colors of turf

--- src/main/java/lykrast/turf/Turf.java
+++ src/main/java/lykrast/turf/Turf.java
@@ -10,6 +10,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.client.renderer.color.ItemColors;
 import net.minecraft.item.BlockItem;
+import net.minecraft.item.DyeColor;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -43,10 +44,13 @@ public class Turf {
 	@SubscribeEvent
 	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
 		blockitems = new ArrayList<>();
-		event.getRegistry().registerAll(
-				makeBlock("turf", new Block(grassProperties())),
-				makeBlock("white_turf", new Block(grassProperties()))
+		IForgeRegistry<Block> reg = event.getRegistry();
+		reg.registerAll(
+				makeBlock("turf", new Block(grassProperties()))
 				);
+		for (DyeColor color : DyeColor.values()) {
+			reg.register(makeBlock(color.getTranslationKey() + "_turf", new Block(grassProperties())));
+		}
 	}
 
 	@SubscribeEvent
@@ -79,11 +83,56 @@ public class Turf {
 		//Too lazy to AT the colorValue so just copy pasted (and converted to hex) from DyeColor
 		bcolors.register((state, world, pos, tintIndex) -> 0xF9FFFE, Holders.WHITE_TURF);
 		icolors.register((stack, tintIndex) -> 0xF9FFFE, Holders.WHITE_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0xF9801D, Holders.ORANGE_TURF);
+		icolors.register((stack, tintIndex) -> 0xF9801D, Holders.ORANGE_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0xC74EBD, Holders.MAGENTA_TURF);
+		icolors.register((stack, tintIndex) -> 0xC74EBD, Holders.MAGENTA_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x3AB3DA, Holders.LIGHT_BLUE_TURF);
+		icolors.register((stack, tintIndex) -> 0x3AB3DA, Holders.LIGHT_BLUE_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0xFED83D, Holders.YELLOW_TURF);
+		icolors.register((stack, tintIndex) -> 0xFED83D, Holders.YELLOW_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x80C71F, Holders.LIME_TURF);
+		icolors.register((stack, tintIndex) -> 0x80C71F, Holders.LIME_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0xF38BAA, Holders.PINK_TURF);
+		icolors.register((stack, tintIndex) -> 0xF38BAA, Holders.PINK_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x474F52, Holders.GRAY_TURF);
+		icolors.register((stack, tintIndex) -> 0x474F52, Holders.GRAY_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x9D9D97, Holders.LIGHT_GRAY_TURF);
+		icolors.register((stack, tintIndex) -> 0x9D9D97, Holders.LIGHT_GRAY_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x169C9C, Holders.CYAN_TURF);
+		icolors.register((stack, tintIndex) -> 0x169C9C, Holders.CYAN_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x8932B8, Holders.PURPLE_TURF);
+		icolors.register((stack, tintIndex) -> 0x8932B8, Holders.PURPLE_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x3C44AA, Holders.BLUE_TURF);
+		icolors.register((stack, tintIndex) -> 0x3C44AA, Holders.BLUE_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x835432, Holders.BROWN_TURF);
+		icolors.register((stack, tintIndex) -> 0x835432, Holders.BROWN_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x5E7C16, Holders.GREEN_TURF);
+		icolors.register((stack, tintIndex) -> 0x5E7C16, Holders.GREEN_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0xB02E26, Holders.RED_TURF);
+		icolors.register((stack, tintIndex) -> 0xB02E26, Holders.RED_TURF.asItem());
+		bcolors.register((state, world, pos, tintIndex) -> 0x1D1D21, Holders.BLACK_TURF);
+		icolors.register((stack, tintIndex) -> 0x1D1D21, Holders.BLACK_TURF.asItem());
 	}
 	
 	@ObjectHolder(Turf.MODID)
 	public static class Holders {
 		public static final Block TURF = null;
 		public static final Block WHITE_TURF = null;
+		public static final Block ORANGE_TURF = null;
+		public static final Block MAGENTA_TURF = null;
+		public static final Block LIGHT_BLUE_TURF = null;
+		public static final Block YELLOW_TURF = null;
+		public static final Block LIME_TURF = null;
+		public static final Block PINK_TURF = null;
+		public static final Block GRAY_TURF = null;
+		public static final Block LIGHT_GRAY_TURF = null;
+		public static final Block CYAN_TURF = null;
+		public static final Block PURPLE_TURF = null;
+		public static final Block BLUE_TURF = null;
+		public static final Block BROWN_TURF = null;
+		public static final Block GREEN_TURF = null;
+		public static final Block RED_TURF = null;
+		public static final Block BLACK_TURF = null;
 	}
 }
