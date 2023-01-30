60facd460d5c70164d75f3f63d3c4384f58dd1dd
Lykrast <aymericpm@gmail.com>
2023-01-30 23:13:17 +0100

    Small adjustments to the help script

--- helpscripts/gencolors.py
+++ helpscripts/gencolors.py
@@ -4,7 +4,16 @@ import glob
 import os
 
 REPLACE = '$temp$'
+#Vanilla
 COLORS = ['black', 'blue', 'brown', 'cyan', 'gray', 'green', 'light_blue', 'light_gray', 'lime', 'magenta', 'orange', 'pink', 'purple', 'red', 'white', 'yellow']
+#Modded dyes for future reference
+#Currently not doing it because:
+#-have to register block and hide it (since loot tables can't be conditional)
+#-can't optionally put in the tag (and JEI picks it up in recipes and I don't want the unobtainable colors to appear)
+#Mekanism
+#COLORS = ['dark_red', 'aqua']
+#Dyenamics
+#COLORS = ['peach', 'aquamarine', 'fluorescent', 'mint', 'maroon', 'bubblegum', 'lavender', 'persimmon', 'cherenkov']
 files = glob.glob('.\\in\\*')
 for f in files:
 	with open(f, 'r') as template:
